c9662bef8503be833b76cd7fcf1700eedb9055f2
sameera-87 <sameekumarasinghe87@gmail.com>
2025-04-13 13:44:13 +0530

    Backend setup done

---
+++ smart-shopping/src/main/java/com/Sameera/smart_shopping/SmartShoppingApplication.java
@@ -0,0 +1,14 @@
+package com.Sameera.smart_shopping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication
+public class SmartShoppingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartShoppingApplication.class, args);
+	}
+
+}
---
+++ smart-shopping/src/test/java/com/Sameera/smart_shopping/SmartShoppingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Sameera.smart_shopping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartShoppingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
